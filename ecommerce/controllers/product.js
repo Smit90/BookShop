2cfe227335f31b9e3fa3ad7a1788933b8bf1ffda
Smit Patel <smitp90333@gmail.com>
2021-03-12 17:48:32 +0530

    Prodcut model and routes

---
+++ ecommerce/controllers/product.js
@@ -0,0 +1,47 @@
+const Product = require('../models/product')
+const formidable = require('formidable')
+const { errorHandler } = require('../helpers/dbErrorHandler')
+const fs = require('fs')
+const _ = require('lodash')
+
+exports.create = (req, res) => {
+    let form = formidable.IncomingForm()
+    form.keepExtension = true
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+
+        const { name, description, price, category, quantity, shipping } = fields
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields are required"
+            })
+        }
+
+
+        let product = new Product(fields)
+
+        if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result)
+        })
+    })
+}
--- ecommerce/models/user.js
+++ ecommerce/models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const crypto = require('crypto')
 const { v1: uuidv1 } = require('uuid')
-const { timeStamp } = require('console')
 
 const userSchema = mongoose.Schema({
     name: {
