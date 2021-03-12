8646a4b4e2a01f05ec04b5e275153a251fae3b0a
Smit Patel <smitp90333@gmail.com>
2021-03-12 18:19:14 +0530

    Product crud operation

--- ecommerce/controllers/product.js
+++ ecommerce/controllers/product.js
@@ -4,7 +4,79 @@ const { errorHandler } = require('../helpers/dbErrorHandler')
 const fs = require('fs')
 const _ = require('lodash')
 
+
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: "Product not found"
+            })
+        }
+        req.product = product
+        next()
+    })
+}
+
+exports.read = (req, res) => {
+    req.product.photo = undefined
+    return res.json(req.product)
+}
+
 exports.create = (req, res) => {
+    let form = formidable.IncomingForm()
+    form.keepExtension = true
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+
+        const { name, description, price, category, quantity, shipping } = fields
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields are required"
+            })
+        }
+
+
+        let product = new Product(fields)
+
+        if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result)
+        })
+    })
+}
+
+exports.remove = (req, res) => {
+    let product = req.product
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json({ "message": "Product deleted successfully" })
+    })
+}
+
+exports.update = (req, res) => {
     let form = formidable.IncomingForm()
     form.keepExtension = true
     form.parse(req, (err, fields, files) => {
