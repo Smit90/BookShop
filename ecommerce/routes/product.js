64a8772a53c937a60e7b5a005dd8bef3c089146c
Smit Patel <smitp90333@gmail.com>
2021-03-13 17:07:12 +0530

    Product queries

--- ecommerce/routes/product.js
+++ ecommerce/routes/product.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-const { create, productById, read, remove, update } = require('../controllers/product')
+const { create, productById, list, read, remove, update, listRelated, listCategories, listBySearch, photo } = require('../controllers/product')
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 router.get('/product/:productId', read)
+router.get('/products', list)
+router.get('/products/related/:productId', listRelated)
+router.get('/products/categories', listCategories)
+router.get('/product/photo/:productId', photo)
+router.post('/products/by/search', listBySearch)
+
 router.post('/product/create/:userId', requireSignin, isAdmin, isAuth, create)
 router.delete('/product/:productId/:userId', requireSignin, isAdmin, isAuth, remove)
 router.put('/product/:productId/:userId', requireSignin, isAdmin, isAuth, update)
