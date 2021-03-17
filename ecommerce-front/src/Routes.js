a88c1e3ec5829bcc46fe2b06110bdd33cdc20994
Smit Patel <smitp90333@gmail.com>
2021-03-17 18:27:04 +0530

    User and Admin Dashboard

--- ecommerce-front/src/Routes.js
+++ ecommerce-front/src/Routes.js
@@ -2,6 +2,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Signin from './user/Signin'
 import Signup from './user/Signup'
 import Home from './core/Home'
+import PrivateRoute from './auth/PrivateRoutes'
+import AdminRoute from './auth/AdminRoute'
+import Dashboard from './user/UserDashboard'
+import AdminDashboard from './user/AdminDashboard'
 
 
 const Routes = () => {
@@ -11,6 +15,8 @@ const Routes = () => {
                 <Route path='/' exact component={Home} />
                 <Route path='/signin' exact component={Signin} />
                 <Route path='/signup' exact component={Signup} />
+                <PrivateRoute path='/user/dashboard' exact component={Dashboard} />
+                <AdminRoute path='/admin/dashboard' exact component={AdminDashboard} />
             </Switch>
         </BrowserRouter>
     );
