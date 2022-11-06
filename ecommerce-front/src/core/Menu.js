b7f3fd9843be934c914aced385e2705c17d492de
Smit Patel <45687440+Smit90@users.noreply.github.com>
2022-11-06 16:58:00 +0530

    add to cart

--- ecommerce-front/src/core/Menu.js
+++ ecommerce-front/src/core/Menu.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { Link, withRouter } from 'react-router-dom'
 import { signout, isAuthenticated } from '../auth/index'
+import { itemTotal } from './cartHelpers'
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) {
@@ -18,6 +19,9 @@ const Menu = ({ history }) => (
             </li>
             <li className="nav-item">
                 <Link className="nav-link" style={isActive(history, '/shop')} to='/shop'>Shop</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" style={isActive(history, '/cart')} to='/cart'>Cart <sup><small className="cart-badge">{itemTotal()}</small></sup></Link>
             </li>
             {isAuthenticated() && isAuthenticated().user.role === 0 && (
                 <li className="nav-item">
